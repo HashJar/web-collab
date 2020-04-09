fe8b00342fcb55ea06d3715c92bd4e2d09aec7e4
HashJar <hash.jar.fh@gmail.com>
2020-04-09 13:48:52 +0530

    Add notebox to the marker on adding comments

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,18 +1,46 @@
 var inject = (function() {
     
-    function addMarker(position) {
+    var lastMarkerId;
+
+    function addMarker(position, markerId) {
         var marker;
         marker = document.createElement('div');
         marker.setAttribute('style', 'top:' + position.y + 'px;left:' + position.x + 'px');
         marker.setAttribute('class', 'web-collab-marker');
+        marker.setAttribute('id', markerId);
         document.body.appendChild(marker);
+    }
+
+    function newMarkerId() {
+        if ('undefined' == typeof lastMarkerId) {
+            lastMarkerId = 0;
+        }
+        lastMarkerId += 1;
+
+        return 'web-collab-marker-' + lastMarkerId.toString();
+    }
+
+    function askForNote(markerId) {
+        var marker, notebox;
+        marker = document.getElementById(markerId);
+        marker.classList.add("active");
+        notebox = document.createElement('textarea');
+        marker.appendChild(notebox);
+    }
+
+    function handleContextMenuClick(position) {
+        var markerId, note;
+        markerId = newMarkerId();
+        addMarker(position, markerId);
+        setTimeout(function() { askForNote(markerId) }, 500);
     }
 
     function addMessageListener() {
         chrome.runtime.onMessage.addListener(
           function(request, sender, sendResponse) {
             console.log(request);
-            addMarker(request.position);
+            handleContextMenuClick(request.position);
+
             sendResponse({'message': 'Thanks!'});
           });
     }
